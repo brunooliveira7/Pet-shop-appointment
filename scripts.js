032790cd95f54139fe5cd5e6a038ef22aa777c9c
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-04-24 21:01:47 -0300

    implementing window transactions via JS

--- scripts.js
+++ scripts.js
@@ -1,5 +1,24 @@
-const newSchedule = document.querySelector('.scheduling-button');
+const newSchedule = document.querySelector(".scheduling-button");
+const schedule = document.querySelector(".schedule-button");
+const closeSchedule = document.querySelector(".close");
+const fade = document.querySelector(".fade");
+const modal = document.querySelector(".modal-overlay");
 
-newSchedule.addEventListener('click', () => {
-    console.log('clicked');
+const toggleModal = () => {
+  modal.classList.toggle("hidden");
+  fade.classList.toggle("hidden");
+};
+
+// Abre o modal
+schedule.addEventListener("click", (event) => {
+  event.preventDefault();
+  toggleModal();
+});
+
+// Fecha o modal (clicando nos elementos que devem fechar)
+[newSchedule, closeSchedule, fade].forEach((element) => {
+  element.addEventListener("click", (event) => {
+    event.preventDefault();
+    toggleModal();
+  });
 });
